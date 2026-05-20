18a9c8f0cc2f8882366e1f3ef30f202c22f17ab3
M.Usman Iqbal <usmaniqbal786804@gmail.com>
2026-05-20 11:11:58 +0500

    Add files via upload

---
+++ lesson1/hello ai .py
@@ -0,0 +1,22 @@
+print("Hello! I am your Ai Bot. What's your Good name? : ")
+
+name=input()
+
+print(f"Nice To meet you,{nam}!")
+
+print("How are you feeling today?(good/bad):")
+
+mood=iput().lower()
+
+if mood=="good":
+
+    print("Glad to hear that!")
+
+elif mood=="bad":
+
+    print("I am sorry to hear that. Hope things get better soon.")
+
+else: 
+    print("I see. sometime it's hard to put feelings into words.")
+
+print(f"It was nice chatting with you {name}. Goodbye!")
